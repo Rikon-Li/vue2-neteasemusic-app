49a8ed3ea74381ebd98ea40ca9acc3773cdd8bb1
李根 <ligen@ligendeMac-mini.local>
2020-09-11 22:25:00 +0800

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <header>云音乐</header>
+    <header>
+      <span class="iconfont icon-caidan"></span>
+      <span>云音乐</span>
+      <span class="iconfont icon-41"></span>
+    </header> 
     <nav>
       <li></li>
       <li></li>
